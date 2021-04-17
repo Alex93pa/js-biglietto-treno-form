256d119e7b50d4fdd003dbd2c67f57328ff8a307
Alex93pa <Ale.pug2@gmail.com>
2021-04-17 02:04:20 +0200

    continuare js

--- script.js
+++ script.js
@@ -1,21 +1,50 @@
+// al click del bottone "genera"
+// var UserNameElement = undefined;
+// var userName = undefined;
+var creaTicket = document.getElementById('crea-ticket');
+var reset = document.getElementById('reset');
+// reset.addEventListener('click', function (){
+//     console.log('PROVA RESET');
+// });
+creaTicket.addEventListener('click', function () {
+    // prezzoKm = 0.21;
 
-function prezzoKm(){
-    prezzoKm = 0.21;
-    return prezzoKm;
-}
+    var UserNameElement = document.getElementById('input-nome');
+    var userName = UserNameElement.value;
+    console.log(userName);
 
+    document.getElementById('nome-passeggero').innerHTML = userName;
 
-var prezzo = kmPercorsi * 0.21
-            
-    if ( anni < 18){
+    var kmPercorsi = document.getElementById("kmUtente").value;
+
+    var anni = document.getElementById("categoria").value;
+
+    var prezzo = kmPercorsi * 0.21
+       
+    if ( anni === "Under18" ){
         var prezzoFinale = prezzo * 0.80
         console.log("prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
-        document.getElementById('costo').innerHTML = "prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
-    } else if ( anni > 65) {
+        document.getElementById('costoBiglietto').innerHTML = prezzoFinale.toFixed( 2 ) + "€";
+    } else if ( anni === "Over65") {
         var prezzoFinale = prezzo * 0.60
         console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
-        document.getElementById('costo').innerHTML = "prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€";
+        document.getElementById('costoBiglietto').innerHTML = prezzoFinale.toFixed( 2 ) + "€";
     } else {
         console.log(prezzo.toFixed( 2 ) + "€")
-        document.getElementById('costo').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
+        document.getElementById('costoBiglietto').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
     } 
+
+
+
+    function randomNumberCarrozza() {
+        var randomNumber = Math.floor(Math.random() *10);
+        return randomNumber;
+    }
+    var numeroCarrozza = randomNumberCarrozza(1, 10);
+    console.log(numeroCarrozza);
+    document.getElementById('carrozza').innerHTML = numeroCarrozza;
+
+
+
+
+});
