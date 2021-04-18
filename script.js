dc9556d4f4824dcd6ea2fbef094ee01747822c8a
Alex93pa <Ale.pug2@gmail.com>
2021-04-18 19:19:56 +0200

    migliorare css

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 // al click del bottone "genera"
-// var UserNameElement = undefined;
-// var userName = undefined;
+
 var creaTicket = document.getElementById('crea-ticket');
 var reset = document.getElementById('reset');
-// reset.addEventListener('click', function (){
-//     console.log('PROVA RESET');
-// });
+
 creaTicket.addEventListener('click', function () {
-    // prezzoKm = 0.21;
+
+    // inserimento nome passeggero nel biglietto
 
     var UserNameElement = document.getElementById('input-nome');
     var userName = UserNameElement.value;
@@ -15,6 +13,8 @@ creaTicket.addEventListener('click', function () {
 
     document.getElementById('nome-passeggero').innerHTML = userName;
 
+
+    //calcolo del prezzi secondo l'età dell'utente
     var kmPercorsi = document.getElementById("kmUtente").value;
 
     var anni = document.getElementById("categoria").value;
@@ -25,26 +25,41 @@ creaTicket.addEventListener('click', function () {
         var prezzoFinale = prezzo * 0.80
         console.log("prezzo scontato under 18 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
         document.getElementById('costoBiglietto').innerHTML = prezzoFinale.toFixed( 2 ) + "€";
+        document.getElementById("offerta").innerHTML = ("Under 18")
     } else if ( anni === "Over65") {
         var prezzoFinale = prezzo * 0.60
         console.log("prezzo scontato over 65 =" + " " + prezzoFinale.toFixed( 2 ) + "€");
         document.getElementById('costoBiglietto').innerHTML = prezzoFinale.toFixed( 2 ) + "€";
+        document.getElementById("offerta").innerHTML = ("Over 65")
     } else {
         console.log(prezzo.toFixed( 2 ) + "€")
         document.getElementById('costoBiglietto').innerHTML = prezzo.toFixed( 2 ) + " " + "€";
+        document.getElementById("offerta").innerHTML = ("Standard")
     } 
 
 
-
+    //numero Carrozza
     function randomNumberCarrozza() {
-        var randomNumber = Math.floor(Math.random() *10);
+        var randomNumber = Math.floor(Math.random() *10 + 1);
         return randomNumber;
     }
     var numeroCarrozza = randomNumberCarrozza(1, 10);
-    console.log(numeroCarrozza);
+    console.log("carrozza: " + numeroCarrozza);
     document.getElementById('carrozza').innerHTML = numeroCarrozza;
 
-
-
+    //codice CP
+    function randomNumberCodiceCP() {
+        var randomNumberCP = Math.floor(Math.random() *90000);
+        return randomNumberCP;
+    }
+    var numeroCodiceCP = randomNumberCodiceCP(90000, 100000);
+    console.log("codice CP: " + numeroCodiceCP);
+    document.getElementById('codice-CP').innerHTML = numeroCodiceCP;
 
 });
+
+//al click del bottone annulla
+
+function reset() {
+    location.reload();
+}
